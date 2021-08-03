e83139e17e2aceb20cb2e957c0b3b7c1ee7468c9
First Last <kiranpg94gmail@ip-172-31-66-198.ec2.internal>
2021-08-03 17:48:51 +0000

    correction 2 in first commit

--- samplecodeprojectclass.java
+++ samplecodeprojectclass.java
@@ -3,7 +3,7 @@ public class samplecodeprojectclass {
 	
 		public static void main(String[] args) {
 			System.out.println("Hello world, I will be a devops engineer!!!");
-			System.out.println("hope my autobuild works!!!!");
+			System.out.println("hope my autobuild works!!!");
 
 }
 }
